eee99507340392067bfc70c191862229a7eccc25
Alexey Yamoncheryaev <jamoncheryev1@gmail.com>
2022-03-26 14:55:43 +0300

    [fix] player controls

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -8,16 +8,34 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Vector3 offset;
     [SerializeField] private float rotateSpeed;
     [SerializeField] private PlayerInput playerInput;
+    [SerializeField] [Range(0, 1)] private float sensitive;
 
     private float smoothTime = 0.01f;
-    private Vector3 velocity = Vector3.zero;
+    private Vector3 zero = Vector3.zero;
+    private float speed;
+    private Vector3 rotateInput;
+
+    void Start()
+    {
+        speed = rotateSpeed * Time.deltaTime;
+    }
 
     void LateUpdate()
     {
-        offset = Quaternion.AngleAxis(
-            playerInput.actions["Look"].ReadValue<Vector2>().x * rotateSpeed, Vector3.up) * offset;
+        rotateInput = playerInput.actions["Rotation"].ReadValue<Vector2>();
+        Debug.Log(rotateInput);
+        offset = Quaternion.AngleAxis(rotateSpeed, SwapXY(rotateInput)) * offset;
         Vector3 newCameraPosition = playerInput.transform.position + offset;
         transform.position = Vector3.Lerp(transform.position, newCameraPosition, smoothTime);
         transform.LookAt(playerInput.transform.position);
+        
+    }
+
+    public static Vector2 SwapXY(Vector2 vector2)
+    {
+        Vector2 newVector2;
+        newVector2.x = vector2.y;
+        newVector2.y = vector2.x;
+        return newVector2;
     }
 }
--- Assets/_Scripts/Player/PlayerMovementController.cs
+++ Assets/_Scripts/Player/PlayerMovementController.cs
@@ -5,30 +5,49 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    [SerializeField] private float playerSpeed = 2.0f;
-    [SerializeField] private float rotateSpeed = 2.0f;
+    [SerializeField] [Range(0, 10f)] private float playerSpeed = 2.0f;
+    [SerializeField] private float gravityValue = -12;
 
-    [SerializeField] private Transform cameraTransform;
+    private CharacterController characterController;
+    private Transform camera;
 
     private PlayerInput playerInput;
     private float speed;
+    private bool groundedPlayer;
+    private Vector3 gravity;
+
+    Vector3 moveInput;
+    Vector3 moveTo;
+    Vector3 zero = Vector3.zero;
+    private float smoothTime = 0.01f;
 
     private void Start()
     {
         playerInput = GetComponent<PlayerInput>();
+        characterController = GetComponent<CharacterController>();
+        camera = Camera.main.transform;
 
         speed = playerSpeed * Time.deltaTime;
     }
 
+
+
     void Update()
     {
-        Vector3 moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
-        Vector3 moveTo = new Vector3(moveInput.x, 0, moveInput.y);
-
-        transform.Translate(moveTo * speed);
+        groundedPlayer = characterController.isGrounded;
+        if (groundedPlayer && gravity.y < 0)
+        {
+            gravity.y = 0f;
+        }
         
-        Vector3 rotateInput = playerInput.actions["Look"].ReadValue<Vector2>();
+        gravity.y += gravityValue * Time.deltaTime;
+        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        Vector3 forward = Vector3.Project(camera.forward, Vector3.forward);
+        moveTo = Vector3.Project(camera.forward, Vector3.forward) * moveInput.y + camera.right * moveInput.x;
+        transform.forward = Vector3.Lerp(transform.forward, Vector3.Project(camera.forward, Vector3.forward),
+            smoothTime);
 
-        transform.Rotate(Vector3.up * rotateSpeed * rotateInput.x);
+        characterController.Move(moveTo * speed);
+        characterController.Move(gravity * Time.deltaTime);
     }
 }
